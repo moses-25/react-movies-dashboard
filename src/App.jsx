3f2447baf798ed5f74a9d8bc7cee63e6019c00c8
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-06-19 19:38:32 +0300

    initialised my app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+// import the react router
+import { BrowserRouter } from "react-router-dom";
+// import the components
 import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard";
+// import the AppRouter
+import AppRoutes from "./routes/AppRoutes";
+// import the styles
 import "./styles/Dashboard.css";
+import "./styles/Details.css";
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Dashboard />
-    </>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
