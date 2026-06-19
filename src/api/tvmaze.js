0e12d5e38eddd1ca70999ae11ef6c0b63ef0c757
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-06-19 19:39:03 +0300

    modified my API url  to base URL

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-const API_URL = "https://api.tvmaze.com/search/shows";
+const BASE_URL = "https://api.tvmaze.com";
 
 export const searchShows = async (query) => {
-    const response = await axios.get(`${API_URL}?q=${query}`);
+    const response = await axios.get(`${BASE_URL}/search/shows?q=${query}`)
+    return response.data;
+};
+
+export const getShowById = async (id) => {
+    const response = await axios.get(`${BASE_URL}/shows/${id}`);
     return response.data;
 };
