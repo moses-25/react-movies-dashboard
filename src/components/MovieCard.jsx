efd6bbdcef23b1e7603d03a68c2a8a9ebda1f36a
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-06-19 19:39:30 +0300

    initialised my moviecard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 export default function MovieCard({ movie }) {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => { navigate(`/movie/${movie.show.id}`); };
+
     return (
-        <div className="movie-card">
-            <img
-                src={movie.show.image?.medium}
-                alt={movie.show.name}
-            />
+        <div className="movie-card" onClick={handleClick}>
+            <img src={movie.show.image?.medium} alt={movie.show.name} />
 
             <div className="movie-content">
                 <h5>{movie.show.name}</h5>
 
                 <p>
                     Rating:
+                    ⭐
                     {" "}
                     {movie.show.rating.average || "N/A"}
                 </p>
