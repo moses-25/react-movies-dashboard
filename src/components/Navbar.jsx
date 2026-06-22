d5f18220abbcbb15d51cf7246e6b2e795973f41d
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-06-22 14:34:48 +0300

    Add routing links (Home, About) to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
-export default function Navbar () {
-    return (
-        <nav className="navbar navbar-dark bg-dark">
-            <div className="container">
-                <span className="navbar-brand">
-                    React Movies Dashboard
-                </span>
-            </div>
-        </nav>
-    );
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+              
+              {/*  here i will add the Logo */} 
+        <Link className="navbar-brand" to="/">
+          React Movies Dashboard
+        </Link>
+
+        <div className="navbar-nav ms-auto">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </div>
+
+      </div>
+    </nav>
+  );
 }
