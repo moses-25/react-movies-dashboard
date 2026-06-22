b86ff3d7874136743058cc1331743a08a27afaeb
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-06-22 14:45:03 +0300

    Add technology icons, Bootstrap grid, and Back to Home button

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,26 +1,34 @@
+import { Link } from "react-router-dom";
+import { FaReact, FaRoute, FaCloud, FaBootstrap, FaTv } from "react-icons/fa";
+import "../styles/Aboutpage.css";
 import TechnologyCard from "../components/TechnologyCard";
 
 function AboutPage() {
   const technologies = [
     {
       name: "React",
-      description: "JavaScript library for building user interfaces."
+      description: "JavaScript library for building user interfaces.",
+      icon: FaReact
     },
     {
       name: "React Router",
-      description: "Provides navigation between pages."
+      description: "Provides navigation between pages.",
+      icon: FaRoute
     },
     {
       name: "Axios",
-      description: "Makes HTTP requests to APIs."
+      description: "Makes HTTP requests to APIs.",
+      icon: FaCloud
     },
     {
       name: "Bootstrap",
-      description: "Responsive CSS framework."
+      description: "Responsive CSS framework.",
+      icon: FaBootstrap
     },
     {
       name: "TVMaze API",
-      description: "Provides TV show information."
+      description: "Provides TV show information.",
+      icon: FaTv
     }
   ];
 
@@ -33,16 +41,19 @@ function AboutPage() {
         using data from the TVMaze API.
       </p>
 
-      <div className="technology-grid">
+      <div className="row row-cols-1 row-cols-md-3 g-4 my-4">
         {technologies.map((tech, index) => (
-          <TechnologyCard
-            key={index}
-            name={tech.name}
-            description={tech.description}
-          />
+          <div className="col" key={index}>
+            <TechnologyCard
+              name={tech.name}
+              description={tech.description}
+              icon={tech.icon}
+            />
+          </div>
         ))}
       </div>
-      
+
+      <Link to="/" className="btn btn-primary mt-3">&#8592; Back to Home</Link>
     </div>
   );
 }
