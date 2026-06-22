6a838d2f0b5fef89c3eca0a2a9e4cb1c430b1856
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-06-22 14:34:50 +0300

    Add About page route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,16 @@
+// import the react router
 import { Routes, Route } from "react-router-dom";
+// import the pages
 import Dashboard from "../pages/Dashboard";
 import Detailpage from "../pages/Detailpage";
+import AboutPage from "../pages/AboutPage";
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/movie/:id" element={<Detailpage />} />
+            <Route path="/about" element={<AboutPage />} />
         </Routes>
     );
 }
